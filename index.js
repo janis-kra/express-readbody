6d66bda53b6f9c571e33afac1c289827536bba12
Janis Krasemann <janis.krasemann@enpit.de>
2016-09-18 19:14:54 +0200

    implement readBody function

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = function readBody (req) {
+  return new Promise((resolve, reject) => {
+    let body = '';
+
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+
+    req.on('end', () => resolve(body));
+    req.on('error', (e) => reject(e));
+  });
+};
